adb9f63672a8f80f42032ee4ed8156560623eb8d
DarkWard <45298792+DarkWard@users.noreply.github.com>
2021-06-09 16:17:15 +0300

    Add files via upload

---
+++ HomeTask_3_4/HomeTask_3_4/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeTask_3_4
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string AddressLine { get; set; }
+    }
+}
