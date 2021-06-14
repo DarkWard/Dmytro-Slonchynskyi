d1456ea6a1e6abc108d009176903db85c6e7eeb2
DarkWard <45298792+DarkWard@users.noreply.github.com>
2021-06-14 17:16:41 +0300

    Delete HomeTask_3_5 directory

--- HomeTask_3_5/HomeTask_3_5/Bridge/IMusicBox.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HomeTask_3_5.Bridge
-{
-    public interface IMusicBox
-    {
-        void Play();
-        void Stop();
-    }
-}
--- HomeTask_3_5/HomeTask_3_5/Visitor/Visitors/IVisitor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HomeTask_3_5.Visitor
-{
-    public interface IVisitor
-    {
-        void VisitGoogleUser(GoogleUser user);
-        void VisitPhoneUser(PhoneUser user);
-        void VisitLoginUser(LoginUser user);
-    }
-}
