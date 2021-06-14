aaec9034676752e86908a8228d038bfed6dfc7ca
DarkWard <45298792+DarkWard@users.noreply.github.com>
2021-06-14 17:18:18 +0300

    Add files via upload

---
+++ HomeTask_3_6/HomeTask_3_6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+
+namespace HomeTask_3_6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var queue = new ConcurrentQueue<int>();
+            var pub1 = new Publisher();
+            var pub2 = new Publisher();
+
+            var sub = new Subscriber(1, pub1);
+
+            var thr1 = new Thread(() => pub1.Publish(queue));
+            var thr2 = new Thread(() => pub2.Publish(queue));
+            thr1.Start();
+            thr2.Start();
+        }
+    }
+}
