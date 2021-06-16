14569f9e5cc24674bfa1ebb841b998414e43df82
DarkWard <45298792+DarkWard@users.noreply.github.com>
2021-06-16 15:31:50 +0300

    Add files via upload

---
+++ HomeTask_3_6/HomeTask_3_6/Publisher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace HomeTask_3_6
+{
+    public class Publisher
+    {
+        public void Publish(ConcurrentQueue<int> queue)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                queue.Enqueue(new Random().Next());
+            }
+        }
+    }
+}
---
+++ HomeTask_3_6/HomeTask_3_6/Subscriber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace HomeTask_3_6
+{
+    public class Subscriber
+    {
+        private const bool V = true;
+        private readonly ConcurrentQueue<int> _queue;
+
+        public Subscriber(ConcurrentQueue<int> queue)
+        {
+            _queue = queue;
+        }
+
+        public void Write()
+        {
+            while (true)
+            {
+                if (!_queue.IsEmpty)
+                {
+                    int val = 0;
+                    _queue.TryDequeue(out val);
+                    Console.WriteLine(val);
+                }
+            }
+        }
+    }
+}
