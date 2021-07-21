62d8b348c5a7d255d00f5946a2b85c2fe2651395
DarkWard <45298792+DarkWard@users.noreply.github.com>
2021-07-21 21:48:20 +0300

    Add files via upload

---
+++ TwoLinkedNode.cs
@@ -0,0 +1,13 @@
+﻿namespace TwoLinkedList
+{
+    internal class TwoLinkedNode : Node
+    {
+        public TwoLinkedNode(object data)
+            : base(data)
+        {
+        }
+
+        public new TwoLinkedNode Next { get; set; }
+        public TwoLinkedNode Prev { get; set; }
+    }
+}
