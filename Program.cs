cf994ea2bfe1293fb10d8636bc32843057f6d22b
DarkWard <45298792+DarkWard@users.noreply.github.com>
2021-08-03 19:47:15 +0300

    Two-Linked List

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TwoLinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list2 = new TwoLinkedList();
+
+            list2.Add(2);
+            list2.Add(7);
+            list2.Add(2);
+            list2.Add(5);
+            list2.Add(4);
+            list2[2] = 14;
+            list2.AddToEnd(23);
+            list2.AddToBegin(39);
+            list2.DeleteByIndex(2);
+            list2.DeleteByValue(2);
+
+            Console.WriteLine(list2.IndexOf(14));
+            Console.WriteLine("\n\n");
+
+            foreach (var i in list2.ToArray())
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ TwoLinkedList.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace TwoLinkedList
+{
+    public class TwoLinkedList : RefList
+    {
+        private TwoLinkedNode _twoTail;
+
+        public override object this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > _count - 1)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                var temp = _head;
+                for (int i = 0; i < index; i++)
+                {
+                    temp = temp.Next as TwoLinkedNode;
+                }
+
+                return temp.Data;
+            }
+
+            set
+            {
+                var add = new TwoLinkedNode(value);
+                var current = _head as TwoLinkedNode;
+
+                for (int i = 0; i < index; i++)
+                {
+                    current = current.Next as TwoLinkedNode;
+                }
+
+                current.Prev.Next = add;
+                add.Next = current;
+                add.Prev = current.Prev;
+                current.Prev = add;
+
+                _count++;
+            }
+        }
+
+        public override void Add(object value)
+        {
+            if (_head == null)
+            {
+                base.Add(value);
+                _twoTail = _head.Next as TwoLinkedNode;
+                _twoTail.Prev = _head as TwoLinkedNode;
+            }
+            else
+            {
+                TwoLinkedNode tmp = _tail as TwoLinkedNode;
+                base.Add(value);
+                _twoTail = _tail as TwoLinkedNode;
+                _twoTail.Prev = tmp;
+            }
+        }
+
+        public override void AddToEnd(object value)
+        {
+            Add(value);
+        }
+
+        public override void AddToBegin(object value)
+        {
+            if (_head == null)
+            {
+                base.AddToBegin(value);
+            }
+            else
+            {
+                TwoLinkedNode node = _head as TwoLinkedNode;
+                base.AddToBegin(value);
+                (_head as TwoLinkedNode).Prev = node;
+            }
+        }
+
+        public override void DeleteByIndex(int index)
+        {
+            base.DeleteByIndex(index);
+
+            TwoLinkedNode current = _head.Next as TwoLinkedNode;
+            TwoLinkedNode prev = _head as TwoLinkedNode;
+
+            for (int i = 0; i < index + 1; i++)
+            {
+                if (i == index)
+                {
+                    TwoLinkedNode tmp = current.Next as TwoLinkedNode;
+                    tmp.Prev = current.Prev;
+
+                    if (current.Next == null)
+                    {
+                        _twoTail = prev;
+                    }
+
+                    break;
+                }
+
+                prev = current;
+                current = current.Next as TwoLinkedNode;
+            }
+        }
+
+        public override void DeleteByValue(object value)
+        {
+            base.DeleteByValue(value);
+        }
+
+        public override int IndexOf(object value)
+        {
+            return base.IndexOf(value);
+        }
+
+        public override object[] ToArray()
+        {
+            return base.ToArray();
+        }
+    }
+}
